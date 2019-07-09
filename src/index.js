7ce093fe73e2b94154be0052806aa9285c58d591
Herkahahaha <herkahahaha@gmail.com>
2019-07-09 22:48:57 +0700

    menambahakan thunk

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducers from "./store/reducers/rootReducers";
 // untuk binding redux kedalam reactjs
 import { Provider } from "react-redux";
+// menambahkan thunk sebagai middleware
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducers);
+const store = createStore(rootReducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
