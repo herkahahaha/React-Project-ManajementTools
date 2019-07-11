3dfd15bd3a9b2002d6eb1f7aedf03b7c0779dfb8
Herkahahaha <herkahahaha@gmail.com>
2019-07-12 01:21:06 +0700

    setting auth ready

--- src/index.js
+++ src/index.js
@@ -21,18 +21,16 @@ const store = createStore(
   compose(
     applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
     reduxFirestore(fbconfig),
-    reactReduxFirebase(fbconfig)
+    reactReduxFirebase(fbconfig, { attachAuthIsReady: true })
   )
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+  serviceWorker.unregister();
+});
